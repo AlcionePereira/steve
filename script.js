63399e1480b6d24aba8759552254a06fe09d76d6
programas de férias <90060154+AlcionePereira@users.noreply.github.com>
2022-03-03 20:30:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function carregar(){}
+
+  const comp_btn_cadastrar = document.getElementById("btn_cadastrar");
+  const comp_caixa_nome = document.getElementById("caixa_nome");
+  const comp_caixa_email = document.getElementById("caixa_email");
+
+  comp_btn_cadastrar.onclick = () => {
+    const nome = comp_caixa_nome.value;
+    const email = comp_caixa_email.value;
+
+    const mensagem =
+      "Olá " +
+      nome +
+      " obrigado por cadatrar seu email: " +
+      email +
+      " - ...!";
+
+    // console.log(mensagem);
+    alert(mensagem);
+  };
